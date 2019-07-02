63137103fe84a0fc254d7b1b9ff46a919cd287e8
Perluceo <FullStackDevSMU@gmail.com>
2019-07-01 22:54:22 -0500

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+	// 1. Initialize Firebase
+    var firebaseConfig = {
+        apiKey: "AIzaSyD676KvA0q4kQrO27PgD2dDG9-pJ79SIMI",
+        authDomain: "trainscheduler-f24da.firebaseapp.com",
+        databaseURL: "https://trainscheduler-f24da.firebaseio.com",
+        projectId: "trainscheduler-f24da",
+        storageBucket: "trainscheduler-f24da.appspot.com",
+        messagingSenderId: "1021293346579",
+        appId: "1:1021293346579:web:2e39ee40aec595d2"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+
+ 
+
+ 
